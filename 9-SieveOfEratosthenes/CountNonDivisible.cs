bdf89402d190e0adb4416e0850dc7d6fd323acb9
CKG <karol.sinx@gmail.com>
2015-06-25 22:10:28 +0100

    Lesson 9: CountNonDivisible (slow)

---
+++ 9-SieveOfEratosthenes/CountNonDivisible.cs
@@ -0,0 +1,41 @@
+﻿namespace Practice
+{
+    using System.Collections.Generic;
+
+    internal class CountNonDivisible
+    {
+        public int[] Solution(int[] A)
+        {
+            var N = A.Length;
+            var numbers = new HashSet<int>();
+            var countDivisibles = new int[(2 * N) + 1];
+            var countIndivisibles = new int[N];
+
+            foreach (var x in A)
+            {
+                numbers.Add(x);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                var num = A[i];
+                while (num <= 2 * N)
+                {
+                    if (numbers.Contains(num))
+                    {
+                        countDivisibles[num]++;
+                    }
+
+                    num += A[i];
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                countIndivisibles[i] = N - countDivisibles[A[i]];
+            }
+
+            return countIndivisibles;
+        }
+    }
+}
