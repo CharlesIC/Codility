29d24e74ba486ab23892654ca769ffd733b3d3e1
Charles G <karol.sinx@gmail.com>
2015-07-16 22:29:19 +0100

    Code refactoring

--- 9-SieveOfEratosthenes/CountSemiprimes.cs
+++ 9-SieveOfEratosthenes/CountSemiprimes.cs
@@ -1,59 +1,58 @@
 ﻿namespace Practice
 {
     using System;
-    using System.Collections.Generic;
 
     public class CountSemiprimes
-	{
-		public int[] Solution(int N, int[] P, int[] Q)
-		{
+    {
+        public int[] Solution(int N, int[] P, int[] Q)
+        {
             var M = P.Length;
-		    var factors = new int[N + 1];
-		    var cumSemiprimes = new int[N + 1];
-		    var countSemiprimes = new int[M];
+            var factors = new int[N + 1];
+            var cumSemiprimes = new int[N + 1];
+            var countSemiprimes = new int[M];
 
             // Determine minimum prime factors for all elements of the array
-		    var num = 2;
-		    while (num * num <= N)
-		    {
-		        if (factors[num] == 0)
-		        {
-		            var k = num * num;
-		            while (k <= N)
-		            {
-		                if (factors[k] == 0)
-		                {
-		                    factors[k] = num;
-		                }
-		                k += num;
-		            }
-		        }
+            var num = 2;
+            while (num * num <= N)
+            {
+                if (factors[num] == 0)
+                {
+                    var k = num * num;
+                    while (k <= N)
+                    {
+                        if (factors[k] == 0)
+                        {
+                            factors[k] = num;
+                        }
 
-		        num++;
-		    }
+                        k += num;
+                    }
+                }
 
-		    // Determine semiprimes
-		    for (int i = 4; i <= N; i++)
-		    {
-		        cumSemiprimes[i] = cumSemiprimes[i - 1];
+                num++;
+            }
 
-		        if (factors[i] != 0)
-		        {
-		            if (factors[i / factors[i]] == 0)
-		            {
-		                cumSemiprimes[i]++;
-		            }
-		        }
-		    }
+            // Determine semiprimes
+            for (int i = 4; i <= N; i++)
+            {
+                cumSemiprimes[i] = cumSemiprimes[i - 1];
+
+                if (factors[i] != 0)
+                {
+                    if (factors[i / factors[i]] == 0)
+                    {
+                        cumSemiprimes[i]++;
+                    }
+                }
+            }
 
             // Return number of semiprimes in range
-		    for (int query = 0; query < M; query++)
-		    {
-		        countSemiprimes[query] = cumSemiprimes[Q[query]] - cumSemiprimes[P[query] - 1];
-		    }
+            for (int query = 0; query < M; query++)
+            {
+                countSemiprimes[query] = cumSemiprimes[Q[query]] - cumSemiprimes[P[query] - 1];
+            }
 
-		    return countSemiprimes;
-		}
-	}
+            return countSemiprimes;
+        }
+    }
 }
-
