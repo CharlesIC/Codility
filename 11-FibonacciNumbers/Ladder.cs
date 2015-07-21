e53f910811f48c1e2ef3736a95149f951fbc1598
Charles G <karol.sinx@gmail.com>
2015-07-21 09:52:51 +0100

    Lesson 11 :: Initial version

---
+++ 11-FibonacciNumbers/Ladder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Ladder
+    {
+        public int[] Solution(int[] A, int[] B)
+        {
+            var count = new int[A.Length];
+            var maxRungs = 0;
+
+            foreach (var num in A)
+            {
+                maxRungs = Math.Max(maxRungs, num);
+            }
+
+            var fibonacci = GetFibonacciSequence(maxRungs);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                var a = fibonacci[A[i] + 1];
+                var b = Math.Pow(2, B[i]);
+
+                count[i] = (int)(a - Math.Floor(a / b) * b);
+            }
+
+            // 8 4 2 1
+            // 8 = 1000
+            // 2 = 0010
+            // 8 % 2 = 0;
+
+            return count;
+        }
+
+        private double[] GetFibonacciSequence(int n)
+        {
+            var fibonacci = new double[n + 2];
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < fibonacci.Length; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            return fibonacci;
+        }
+    }
+}
