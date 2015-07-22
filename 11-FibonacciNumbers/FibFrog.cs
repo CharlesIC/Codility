20ddf3e73f33fdfca6bd2d6ef8893bbd42480bde
Charles G <karol.sinx@gmail.com>
2015-07-22 09:28:45 +0100

    Lesson 11 :: FibFrog

--- 11-FibonacciNumbers/FibFrog.cs
+++ 11-FibonacciNumbers/FibFrog.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Practice
 {
     public class FibFrog
     {
-        public FibFrog()
+        public int[] solution(int[] A)
         {
-            
+            var positionBegin = -1;
+            var positionEnd = A.Length;
+            var pathLength = positionEnd - positionBegin;
+
+            var fibonacci = GetFibonacciSequence(A.Length + 1);
+
+            return null;
+        }
+
+        private int[] GetFibonacciSequence(int max)
+        {
+            var fibonacci = new List<int>();
+            fibonacci.AddRange(new[] { 0, 1 });
+
+            var idx = 1;
+            while (fibonacci[idx] < max)
+            {
+                idx++;
+                fibonacci.Add(fibonacci[idx - 1] + fibonacci[idx - 2]);
+            }
+
+            return fibonacci.GetRange(0, idx).ToArray();
         }
     }
 }
