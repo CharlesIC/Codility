2fe817b9ebbfd4997970a40d0461aa2bb4e9462f
Charles G <karol.sinx@gmail.com>
2015-07-23 10:11:14 +0100

    FibFrog :: v1

--- 11-FibonacciNumbers/FibFrog.cs
+++ 11-FibonacciNumbers/FibFrog.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections.Generic;
+<<<<<<< Updated upstream
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+>>>>>>> Stashed changes
 
 namespace Codility
 {
     public class FibFrog
     {
+<<<<<<< Updated upstream
         public int[] Solution(int[] A)
+>>>>>>> Stashed changes
         {
-            var positionBegin = -1;
-            var positionEnd = A.Length;
-            var pathLength = positionEnd - positionBegin;
+            const int Origin = -1;
+            var destination = A.Length + 1;
+            var fibonacci = GetFibonacciSequence(destination + 1);
 
-            var fibonacci = GetFibonacciSequence(A.Length + 1);
+            // Perform a breadth-first search on the array
+            var queue = new Queue<Leaf>();
+            queue.Enqueue(new Leaf { Position = Origin, Hops = 0 });
 
-            return null;
+            while (queue.Count > 0)
+            {
+                var leaf = queue.Dequeue();
+                var currentPosition = leaf.Position;
+                var maxDistance = destination - currentPosition;
+
+                foreach (var distance in fibonacci.Where(x => x <= maxDistance))
+                {
+                    var nextPosition = currentPosition + distance;
+
+                    if (nextPosition == destination)
+                    {
+                        return leaf.Hops++;
+                    }
+
+                    if (A[nextPosition] == 1)
+                    {
+                        queue.Enqueue(new Leaf { Position = nextPosition, Hops = leaf.Hops++ });
+                    }
+                }
+            }
+
+            return -1;
         }
 
         private int[] GetFibonacciSequence(int max)
@@ -29,6 +59,11 @@ namespace Codility
 
             return fibonacci.GetRange(0, idx).ToArray();
         }
+
+        private struct Leaf
+        {
+            public int Position;
+            public int Hops;
+        }
     }
 }
-
