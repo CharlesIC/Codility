dbaac00cf4f60bdf9917827f52115944c339bb57
Charles G <karol.sinx@gmail.com>
2015-07-30 22:33:23 +0100

    Lesson 12 :: MinMaxDivison - final version

--- 12-BinarySearch/MinMaxDivision.cs
+++ 12-BinarySearch/MinMaxDivision.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Codility
 {
@@ -81,6 +82,70 @@ namespace Codility
         private int SumBetweenIndices(int[] prefixSums, int a, int b)
         {
             return prefixSums[b + 1] - prefixSums[a];
+        }
+
+
+        public int Solution2(int K, int M, int[] A)
+        {
+            var blocksNeeded = 0;
+
+            var result = 0;
+            var lowerBound = A.Max();
+            var upperBound = A.Sum();
+
+            // Handle special cases
+            if (K == 1)
+            {
+                return upperBound;
+            }
+
+            if (K >= A.Length)
+            {
+                return lowerBound;
+            }
+                
+            // Binary search for the result
+            while (lowerBound <= upperBound)
+            {
+                var mid = (lowerBound + upperBound) / 2;
+                blocksNeeded = GetBlocksNeeded(A, mid);
+
+                if (blocksNeeded <= K)
+                {
+                    upperBound = mid - 1;
+                    result = mid;
+                }
+                else
+                {
+                    lowerBound = mid + 1;
+                }
+            }
+
+            return result;
+        }
+
+        private int GetBlocksNeeded(int[] A, int maxBlockSum)
+        {
+            var countBlocks = 1;
+            var preBlockSum = A[0];
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                // Try to extend the previous block
+                if (preBlockSum + A[i] > maxBlockSum)
+                {
+                    // Can't extend the previous block because
+                    // it would exceed the max allowed sum
+                    preBlockSum = A[i];
+                    countBlocks++;
+                }
+                else
+                {
+                    preBlockSum += A[i];
+                }
+            }
+
+            return countBlocks;
         }
     }
 }
