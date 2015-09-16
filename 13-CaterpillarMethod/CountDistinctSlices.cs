cc9a17fd6699b61cd541db4d1cca38ba2883c94c
Charles G <karol.sinx@gmail.com>
2015-09-16 21:29:16 +0100

    Lesson 13 :: DistinctSlices intermediate code

--- 13-CaterpillarMethod/CountDistinctSlices.cs
+++ 13-CaterpillarMethod/CountDistinctSlices.cs
@@ -10,70 +10,6 @@ namespace Codility
             const int limit = (int)1e9;
             var result = 0;
 
-            for (int back = 0; back < A.Length; back++)
-            {
-                var elements = new HashSet<int>();
-
-                var front = back;
-                while (front < A.Length && !elements.Contains(A[front]) && result < limit)
-                {
-                    elements.Add(A[front]);
-                    front++;
-                } 
-                    
-                var n = front - back;
-                result += (n + 1) * n / 2;
-               
-                if (result >= limit)
-                {
-                    return limit;
-                }
-
-                back = front - 1;
-            }
-
-            return Math.Min(limit, result);
-        }
-
-        public int solution2(int M, int[] A)
-        {
-            const int limit = (int)1e9;
-            var result = 0;
-
-            var back = 0;
-            while (back < A.Length)
-            {
-                var elements = new HashSet<int>();
-
-                var front = back;
-                while (front < A.Length && !elements.Contains(A[front]) && result < limit)
-                {
-                    elements.Add(A[front]);
-                    front++;
-                }
-
-                var n = front - back;
-                result += (n + 1) * n / 2;
-
-                if (result >= limit)
-                {
-                    return limit;
-                }
-
-                back = 
-                    front - back > 1 ?
-                    front - 1 : 
-                    back + 1;
-            }
-
-            return Math.Min(limit, result);
-        }
-
-        public int solution3(int M, int[] A)
-        {
-            const int limit = (int)1e9;
-            var result = 0;
-
             var back = 0;
             while (back < A.Length)
             {
@@ -103,7 +39,123 @@ namespace Codility
                 }
             }
 
-            return result < limit ? result : limit;
+            return Math.Min(result, limit);
+        }
+
+        public int solution2(int M, int[] A)
+        {
+            const int limit = (int)1e9;
+            var result = 0;
+
+            var back = 0;
+            while (back < A.Length)
+            {
+                var elements = new HashSet<int>();
+
+                var front = back;
+                while (front < A.Length && !elements.Contains(A[front]) && result < limit)
+                {
+                    elements.Add(A[front]);
+                    result += elements.Count;
+                    front++;
+                }
+
+                if (result >= limit)
+                {
+                    return limit;
+                }
+
+                back = Math.Max(front - 1, back + 1);
+            }
+                
+            return Math.Min(result, limit);
+        }
+
+        public int solution3(int M, int[] A)
+        {
+            const int limit = (int)1e9;
+            var result = 0;
+            int back, front;
+
+            back = front = 0;
+            while (back < A.Length && front < A.Length)
+            {
+                var elements = new HashSet<int>();
+
+                front = back;
+                while (front < A.Length && !elements.Contains(A[front]) && result < limit)
+                {
+                    elements.Add(A[front]);
+                    result += elements.Count;
+                    front++;
+                }
+
+                if (front < A.Length)
+                {
+                    if (A[front] == A[front - 1])
+                    {
+                        back = front;
+                    }
+                    else
+                    {
+                        back = front - 1;
+                        result--;
+                    }
+                }
+
+                if (result >= limit)
+                {
+                    return limit;
+                }
+            }
+
+            return Math.Min(result, limit);
+        }
+
+        public int solution4(int M, int[] A)
+        {
+            const int limit = (int)1e9;
+            int back, front;
+            var result = 0;
+
+            var elements = new HashSet<int>();
+
+            front = 0;
+            while (front < A.Length)
+            {
+                if (!elements.Contains(A[front]))
+                {
+                    elements.Add(A[front]);
+                    result += elements.Count;
+                    front++;
+                }
+                else
+                {
+                    elements.Clear();
+
+                    if (A[front] == A[front - 1])
+                    {
+                        back = front;
+                    }
+                    else
+                    {
+                        back = --front;
+                        result--;
+                    }
+
+                    if (result >= limit)
+                    {
+                        return limit;
+                    }
+                }
+            }
+
+            return Math.Min(result, limit);
+        }
+
+        public int solution5(int M, int[] A)
+        {
+            return -1;
         }
     }
 }
