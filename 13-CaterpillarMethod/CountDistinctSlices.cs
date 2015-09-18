37d522c569bcd47e7e16c195325abef859561a9f
Charles G <karol.sinx@gmail.com>
2015-09-18 22:51:54 +0100

    Lesson 13 :: CountDistinctSlices final

--- 13-CaterpillarMethod/CountDistinctSlices.cs
+++ 13-CaterpillarMethod/CountDistinctSlices.cs
@@ -39,40 +39,5 @@ namespace Codility
 
             return Math.Min(result, limit);
         }
-
-        public int solution2(int M, int[] A)
-        {
-            const int limit = 1000000000;
-            var elements = new HashSet<int>();
-            int back, front, result;
-            back = front = result = 0;
-
-            while (front < A.Length && back < A.Length)
-            {
-                while (front < A.Length && !elements.Contains(A[front]))
-                {
-                    result += (front - back + 1);
-
-                    if (result >= limit)
-                    {
-                        return limit;
-                    }
-
-                    elements.Add(A[front]);
-                    front++;
-                }
-
-                while (front < A.Length && back < A.Length && A[back] != A[front])
-                {
-                    elements.Remove(A[back]);
-                    back++;
-                }
-
-                elements.Remove(A[back]);
-                back++;
-            }
-
-            return Math.Min(result, limit);
-        }
     }
 }
