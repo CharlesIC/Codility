a6fc1544451ac77f7a10d9f84d6757d829197db6
Charles G <karol.sinx@gmail.com>
2015-09-20 00:47:55 +0100

    Lesson 13 :: CountTriangles

---
+++ 13-CaterpillarMethod/CountTriangles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Codility
+{
+    public class CountTriangles
+    {
+        public int solution(int[] A)
+        {
+            var result = 0;
+
+            Array.Sort(A);
+
+            for (int x = 0; x < A.Length; x++)
+            {
+                var z = 0;
+                for (int y = x + 1; y < A.Length; y++)
+                {
+                    while (z < A.Length && A[x] + A[y] > A[z])
+                    {
+                        z++;
+                    }
+
+                    result += z - y - 1;
+                }
+            }
+                
+            return result;
+        }
+    }
+}
