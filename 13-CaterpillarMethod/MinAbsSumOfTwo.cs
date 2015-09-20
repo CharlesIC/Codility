36749e1cd4225f4ed7d460187d89cbbecb862188
Charles G <karol.sinx@gmail.com>
2015-09-20 17:19:01 +0100

    Lesson 13 :: MinAbsSumOfTwo alternative

--- 13-CaterpillarMethod/MinAbsSumOfTwo.cs
+++ 13-CaterpillarMethod/MinAbsSumOfTwo.cs
@@ -10,6 +10,19 @@ namespace Codility
 
             var tail = 0;
             var head = A.Length - 1;
+
+            // All positive
+            if (A[tail] >= 0)
+            {
+                return A[tail] + A[tail];
+            }
+
+            // All negative
+            if (A[head] <= 0)
+            {
+                return Math.Abs(A[head] + A[head]);
+            }
+
             var result = Math.Abs(A[tail] + A[head]);
 
             while (tail < head)
@@ -55,6 +68,49 @@ namespace Codility
         {
             return tail < head && Math.Abs(A[tail] + A[head - 1]) <= Math.Abs(A[tail] + A[head]);
 
+        }
+
+        public int solution2(int[] A)
+        {
+            Array.Sort(A);
+
+            var tail = 0;
+            var head = A.Length - 1;
+            var result = Math.Abs(A[tail] + A[head]);
+
+            // All positive
+            if (A[tail] >= 0)
+            {
+                return A[tail] + A[tail];
+            }
+
+            // All negative
+            if (A[head] <= 0)
+            {
+                return Math.Abs(A[head] + A[head]);
+            }
+
+            while (tail <= head)
+            {
+                var temp = Math.Abs(A[tail] + A[head]);
+                result = Math.Min(result, temp);
+
+                if (Math.Abs(A[tail + 1] + A[head]) < temp)
+                {
+                    tail++;
+                }
+                else if (Math.Abs(A[tail] + A[head - 1]) < temp)
+                {
+                    head--;
+                }
+                else
+                {
+                    tail++;
+                    head--;
+                }
+            }
+
+            return result;
         }
     }
 }
