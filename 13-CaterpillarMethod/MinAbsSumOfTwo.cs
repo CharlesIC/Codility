a4104d4cc787b56267731141c2578780ce7eeb8e
Charles G <karol.sinx@gmail.com>
2015-09-20 16:38:56 +0100

    Lesson 13 :: MinAbsSumOfTwo v2

--- 13-CaterpillarMethod/MinAbsSumOfTwo.cs
+++ 13-CaterpillarMethod/MinAbsSumOfTwo.cs
@@ -31,7 +31,7 @@ namespace Codility
                         front++;
                     }
 
-                    result = Math.Min(Math.Abs(A[back] + A[front]));
+                    result = Math.Min(result, Math.Abs(A[back] + A[front]));
                 }
 
                 if (back > 0)
@@ -63,6 +63,69 @@ namespace Codility
             if (front < A.Length - 1)
             {
                 return Math.Abs(A[back] + A[front + 1]) <= Math.Abs(A[back] + A[front]);
+            }
+
+            return false;
+        }
+
+        public int solution2(int[] A)
+        {
+            Array.Sort(A);
+
+            var tail = 0;
+            var head = A.Length - 1;
+            var result = Math.Abs(A[tail] + A[head]);
+
+            while (tail != head)
+            {
+                while (CanMoveTail(A, tail, head) || CanMoveHead(A, tail, head))
+                {
+                    while (CanMoveTail(A, tail, head))
+                    {
+                        tail++;
+                    }
+
+                    while (CanMoveHead(A, tail, head))
+                    {
+                        head--;
+                    }
+
+                    result = Math.Min(result, Math.Abs(A[tail] + A[head]));
+                }
+
+                if (tail != head)
+                {
+                    tail++;
+                }
+
+                if (tail != head)
+                {
+                    head--;
+                }
+
+                result = Math.Min(result, Math.Abs(A[tail] + A[head]));
+            }
+
+            return result;
+        }
+
+        private bool CanMoveTail(int[] A, int tail, int head)
+        {
+            if (tail < A.Length - 1)
+            {
+                return 
+                    tail != head && Math.Abs(A[tail + 1] + A[head]) <= Math.Abs(A[tail] + A[head]);
+            }
+
+            return false;
+        }
+
+        private bool CanMoveHead(int[] A, int tail, int head)
+        {
+            if (head > 0)
+            {
+                return
+                    tail != head && Math.Abs(A[tail] + A[head - 1]) <= Math.Abs(A[tail] + A[head]);
             }
 
             return false;
