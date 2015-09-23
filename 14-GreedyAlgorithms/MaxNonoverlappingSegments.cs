f41f9d544c490ef3376992c3f9201601af8d55ac
Charles G <karol.sinx@gmail.com>
2015-09-23 20:15:13 +0100

    Lesson 14 :: MaxNonoverlappingSegments v2

--- 14-GreedyAlgorithms/MaxNonoverlappingSegments.cs
+++ 14-GreedyAlgorithms/MaxNonoverlappingSegments.cs
@@ -8,25 +8,8 @@ namespace Codility
     {
         public int solution(int[] A, int[] B)
         {
-            var startPositions = new Dictionary<int, List<int>>();
-            var endPositions = new Dictionary<int, List<int>>();
-
-            for (int i = 0; i < A.Length; i++)
-            {
-                if (!startPositions.ContainsKey(A[i]))
-                {
-                    startPositions.Add(A[i], new List<int>());
-                }
-
-                if (!endPositions.ContainsKey(B[i]))
-                {
-                    endPositions.Add(B[i], new List<int>());
-                }
-
-                startPositions[A[i]].Add(i);
-                endPositions[B[i]].Add(i);
-            }
-
+            var startPositions = GetIndexedPositions(A);
+           
             var overlaps = new Dictionary<int, int>();
 
             var pool = new List<int>();
@@ -36,7 +19,7 @@ namespace Codility
                 var newSticks = new List<int>();
                 var sticksEnding = new HashSet<int> { i };
 
-                // Get to the end of next stic
+                // Get to the end of next stick
                 while (i < B.Length - 1 && B[i + 1] == B[i])
                 {
                     sticksEnding.Add(i);
@@ -80,6 +63,112 @@ namespace Codility
             }
 
             return -1;
+        }
+
+        public int solution2(int[] A, int[] B)
+        {
+            var result = 0;
+            var sticksAtPosition = GetSticksAtPosition(A, B);
+
+            var start = A.Min();
+            var pool = new HashSet<int>();
+            var excludedSticks = new HashSet<int>();
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                // Get to the end of next stick
+                var sticksEnding = new HashSet<int> { B[i] };
+                while (i < B.Length - 1 && B[i + 1] == B[i])
+                {
+                    i++;
+                    sticksEnding.Add(i);
+                }
+
+                var currentPosition = B[i];
+
+            }
+
+            return -1;
+        }
+
+        private Dictionary<int, List<int>> GetIndexedPositions(int[] A)
+        {
+            var indexedPositions = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (!indexedPositions.ContainsKey(A[i]))
+                {
+                    indexedPositions.Add(A[i], new List<int>());
+                }
+
+                indexedPositions[A[i]].Add(i);
+            }
+
+            return indexedPositions;
+        }
+
+        private Dictionary<int, HashSet<int>> GetSticksAtPosition(int[] A, int[] B)
+        {
+            var n = A.Length;
+            var innerCount = 0;
+            var outerCount = 0;
+
+            var sticksAtPosition = new Dictionary<int, HashSet<int>>();
+            var pool = new HashSet<int>();
+
+            for (int i = B.Length - 1; i >= 0; i--)
+            {
+                outerCount++;
+
+                var currentPosition = B[i];
+                var newPool = new HashSet<int>();
+                pool.Add(i);
+
+                if (!sticksAtPosition.ContainsKey(currentPosition))
+                {
+                    sticksAtPosition.Add(currentPosition, new HashSet<int>());   
+                }
+
+                foreach (var stick in pool)
+                {
+                    var startPosition = A[stick];
+
+                    if (startPosition <= currentPosition)
+                    {
+                        sticksAtPosition[currentPosition].Add(stick);
+                        newPool.Add(stick);
+                    }
+
+                    innerCount++;
+                }
+
+                pool = newPool;
+            }
+
+            return sticksAtPosition;
+        }
+
+        public int solution3(int[] A, int[] B)
+        {
+            var result = 0;
+            var stick = 0;
+
+            while (stick < B.Length)
+            {
+                result++;
+                var currentPosition = B[stick];
+
+                var nextStick = stick + 1;
+                while (nextStick < B.Length && A[nextStick] <= currentPosition)
+                {
+                    nextStick++;
+                }
+
+                stick = nextStick;
+            }
+
+            return result;
         }
     }
 }
