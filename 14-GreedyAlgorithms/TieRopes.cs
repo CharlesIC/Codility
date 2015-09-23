1de99ce321ef7d005b8c54009fa79bd7918bda91
Charles G <karol.sinx@gmail.com>
2015-09-23 20:46:14 +0100

    Lesson 14 :: TieRopes

---
+++ 14-GreedyAlgorithms/TieRopes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Codility
+{
+    public class TieRopes
+    {
+        public int solution(int K, int[] A)
+        {
+            var result = 0;
+            var currentLength = 0;
+
+            foreach (var length in A)
+            {
+                if (currentLength < K)
+                {
+                    currentLength += length;
+                }
+                else
+                {
+                    result++;
+                    currentLength = length;
+                }
+            }
+
+            if (currentLength >= K)
+            {
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
