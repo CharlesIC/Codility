103a2fa66ffbbfe3d7f1bb57fa534eb8cb3092c5
Charles G <karol.sinx@gmail.com>
2015-09-24 20:22:37 +0100

    Lesson 14 :: DynamicFrog (extra)

---
+++ 15-DynamicProgramming/DynamicFrog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice
+{
+    public class DynamicFrog
+    {
+        public int solution(int[] S, int k, int q)
+        {
+            var n = S.Length;
+            var dp = new int[k + 1];
+            dp[0] = 1;
+
+            for (int j = 1; j < k + 1; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (S[i] <= j)
+                    {
+                        dp[j] = (dp[j] + dp[j - S[i]]) % q;
+                    }
+                }
+            }
+
+            return dp[k];
+        }
+    }
+}
