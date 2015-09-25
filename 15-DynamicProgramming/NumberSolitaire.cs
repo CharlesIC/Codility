3bf8d7774f3eddb09ec54b64dd71a6691d77eca8
Charles G <karol.sinx@gmail.com>
2015-09-26 00:59:06 +0100

    Lesson 15 :: NumberSolitaire intermediate

--- 15-DynamicProgramming/NumberSolitaire.cs
+++ 15-DynamicProgramming/NumberSolitaire.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Codility
@@ -13,6 +14,8 @@ namespace Codility
             var result = A[0];
 
             var marked = 0;
+            markedSquares.Push(marked);
+
             var maxInRoundIdx = 1;
 
             for (int square = 1; square < n; square++)
@@ -21,10 +24,18 @@ namespace Codility
                 {
                     marked = maxInRoundIdx;
 
-                    if (A[markedSquares.Peek()] < 0)
+                    if (markedSquares.Count > 1 && A[markedSquares.Peek()] < 0)
                     {
                         var lastMarked = markedSquares.Pop();
 
+                        if (marked - markedSquares.Peek() <= maxDistance)
+                        {
+                            result -= A[lastMarked];
+                        }
+                        else
+                        {
+                            markedSquares.Push(lastMarked);
+                        }
                     }
 
                     markedSquares.Push(marked);
@@ -50,9 +61,39 @@ namespace Codility
             if (A[n - 1] < 0)
             {
                 result += A[n - 1];
+                markedSquares.Push(n - 1);
             }
 
+            Console.Write("\nSelected:");
+            foreach (var square in markedSquares)
+            {
+                Console.Write("{0} ", square);
+            }
+            Console.WriteLine();
+
             return result;
+        }
+
+        public int solution2(int[] A)
+        {
+            const int d = 6;
+            var n = A.Length;
+
+            var maxScore = new int[d];
+            maxScore[0] = A[0];
+
+            for (int i = 1; i < maxScore.Length; i++)
+            {
+                maxScore[i] = int.MinValue;
+            }
+
+
+            for (int p = 1; p < n; p++)
+            {
+                maxScore[p % d] = maxScore.Max() + A[p];
+            }
+
+            return maxScore[(n - 1) % d];
         }
     }
 }
