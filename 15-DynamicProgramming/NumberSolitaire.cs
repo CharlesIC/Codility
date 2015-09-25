0a81a71679db4aa31b790e9d8f66c0f49fad7b19
Charles G <karol.sinx@gmail.com>
2015-09-26 00:59:56 +0100

    Lesson 15 :: NumberSolitaire final

--- 15-DynamicProgramming/NumberSolitaire.cs
+++ 15-DynamicProgramming/NumberSolitaire.cs
@@ -1,80 +1,11 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace Codility
 {
     public class NumberSolitaire
     {
         public int solution(int[] A)
-        {
-            const int maxDistance = 6;
-            var markedSquares = new Stack<int>();
-            var n = A.Length;
-            var result = A[0];
-
-            var marked = 0;
-            markedSquares.Push(marked);
-
-            var maxInRoundIdx = 1;
-
-            for (int square = 1; square < n; square++)
-            {
-                if (square - marked > maxDistance)
-                {
-                    marked = maxInRoundIdx;
-
-                    if (markedSquares.Count > 1 && A[markedSquares.Peek()] < 0)
-                    {
-                        var lastMarked = markedSquares.Pop();
-
-                        if (marked - markedSquares.Peek() <= maxDistance)
-                        {
-                            result -= A[lastMarked];
-                        }
-                        else
-                        {
-                            markedSquares.Push(lastMarked);
-                        }
-                    }
-
-                    markedSquares.Push(marked);
-                    result += A[maxInRoundIdx];
-                    maxInRoundIdx = marked + 1;
-                    square = marked + 1;
-                }
-
-                if (square < n)
-                {
-                    maxInRoundIdx = A[square] >= A[maxInRoundIdx] ? square : maxInRoundIdx;
-
-                    if (A[square] >= 0)
-                    {
-                        marked = square;
-                        markedSquares.Push(marked);
-                        result += A[square];
-                        maxInRoundIdx = square + 1;
-                    }   
-                }
-            }
-
-            if (A[n - 1] < 0)
-            {
-                result += A[n - 1];
-                markedSquares.Push(n - 1);
-            }
-
-            Console.Write("\nSelected:");
-            foreach (var square in markedSquares)
-            {
-                Console.Write("{0} ", square);
-            }
-            Console.WriteLine();
-
-            return result;
-        }
-
-        public int solution2(int[] A)
         {
             const int d = 6;
             var n = A.Length;
