b76a7bf9b80bcfe37a81a3b74c4fd6c57f8f5c3c
Charles G <karol.sinx@gmail.com>
2015-09-30 14:52:28 +0100

    Lesson 15 :: MinAbsSum final

--- 15-DynamicProgramming/MinAbsSum.cs
+++ 15-DynamicProgramming/MinAbsSum.cs
@@ -8,45 +8,6 @@ namespace Codility
     {
         public int solution(int[] A)
         {
-            var n = A.Length;
-            var result = 0;
-
-            foreach (var num in A)
-            {
-                result = Math.Min(Math.Abs(result + num), Math.Abs(result - num));
-            }
-
-            return result;
-        }
-
-        public int solution2(int[] A)
-        {
-            if (A.Length == 0)
-            {
-                return 0;
-            }
-
-            var sums = new List<int> { A[0] };
-
-            for (int i = 1; i < A.Length; i++)
-            {
-                var num = A[i];
-                var newSums = new List<int>();
-
-                foreach (var sum in sums)
-                {
-                    newSums.Add(Math.Abs(sum + num));
-                    newSums.Add(Math.Abs(sum - num));
-                }
-
-                sums = newSums;
-            }
-
-            return sums.Min();
-        }
-
-        public int solution3(int[] A)
-        {
             for (int i = 0; i < A.Length; i++)
             {
                 A[i] = Math.Abs(A[i]);
