c599520e40fe60b94280ee511cc62bcd726e8b14
Charles G <karol.sinx@gmail.com>
2015-10-01 20:20:57 +0100

    Lesson 99 :: TreeHeight

---
+++ 99-FutureTraining/TreeHeight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Codility
+{
+    public class TreeHeight
+    {
+        public int solution(Tree T)
+        { 
+            if (T == null)
+            {
+                return -1;
+            }
+
+            return Math.Max(solution(T.l), solution(T.r)) + 1;
+        }
+    }
+
+    public class Tree
+    {
+        public int x;
+        public Tree l;
+        public Tree r;
+    };
+}
