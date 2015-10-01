c6c8d669461be37d8a1b8f5ecb5bd32e643eafac
Charles G <karol.sinx@gmail.com>
2015-10-01 23:15:03 +0100

    Lesson 99 :: BinaryGap final

--- 99-FutureTraining/BinaryGap.cs
+++ 99-FutureTraining/BinaryGap.cs
@@ -9,57 +9,19 @@ namespace Codility
         {
             int x = N;
             var count = 0;
-            var gap = new Stack<int>();
+            var currentCount = 0;
 
-            while (x > 0)
+            // Skip initial zeros
+            while (x > 0 && x % 2 == 0)
             {
-                var bit = x % 2;
-
-                if (bit == 1)
-                {
-                    if (gap.Count == 0)
-                    {
-                        gap.Push(1);
-                    }
-                    else
-                    {
-                        count = Math.Max(count, gap.Count - 1);
-                        gap.Clear();
-                        gap.Push(1);
-                    }
-                }
-                else
-                {
-                    if (gap.Count > 0)
-                    {
-                        gap.Push(0);
-                    }
-                }
-
                 x /= 2;
             }
 
-            return count;
-        }
-
-        // More memory efficient
-        public int solution2(int N)
-        {
-            int x = N;
-            var count = 0;
-            var currentCount = -1;
-
             while (x > 0)
             {
-                var bit = x % 2;
-
-                if (bit == 1)
+                if (x % 2 == 1)
                 {
-                    if (currentCount == -1)
-                    {
-                        currentCount = 0;
-                    }
-                    else
+                    if (currentCount != 0)
                     {
                         count = Math.Max(count, currentCount);
                         currentCount = 0;
@@ -67,10 +29,7 @@ namespace Codility
                 }
                 else
                 {
-                    if (currentCount >= 0)
-                    {
-                        currentCount++;
-                    }
+                    currentCount++;
                 }
 
                 x /= 2;
