7f3082bb3395408a0f8376401f3cc5b0269197a5
Charles G <karol.sinx@gmail.com>
2015-10-02 00:01:16 +0100

    Lesson 99 :: OddOccurrencesInArray

---
+++ 99-FutureTraining/OddOccurrencesInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Codility
+{
+    public class OddOccurrencesInArray
+    {
+        public int solution(int[] A)
+        {
+            var elements = new HashSet<int>();
+
+            foreach (var num in A)
+            {
+                if (elements.Contains(num))
+                {
+                    elements.Remove(num);
+                }
+                else
+                {
+                    elements.Add(num);
+                }
+            }
+
+            return elements.Single();
+        }
+    }
+}
