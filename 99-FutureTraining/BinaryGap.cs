e77ba47df656b45f51b67e43e9cc6a8621f77d1f
Charles G <karol.sinx@gmail.com>
2015-10-02 00:01:31 +0100

    Lesson 99 :: BinaryGap binary operators solution

--- 99-FutureTraining/BinaryGap.cs
+++ 99-FutureTraining/BinaryGap.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Codility
 {
@@ -33,6 +32,39 @@ namespace Codility
                 }
 
                 x /= 2;
+            }
+
+            return count;
+        }
+
+        public int solution2(int N)
+        {
+            int x = N;
+            int count = 0;
+            var currentCount = 0;
+
+            // Skip initial zeros
+            while ((x & 1) == 0)
+            {
+                x = x >> 1;
+            }
+
+            while (x > 0)
+            {
+                if ((x & 1) == 1)
+                {
+                    if (currentCount != 0)
+                    {
+                        count = Math.Max(count, currentCount);
+                        currentCount = 0;
+                    }
+                }
+                else
+                {
+                    currentCount++;
+                }
+
+                x = x >> 1;
             }
 
             return count;
