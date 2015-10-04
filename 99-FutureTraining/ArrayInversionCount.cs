ec67f781ce66dd6a896a4fdf51cfa1d6cfb12e99
Charles G <karol.sinx@gmail.com>
2015-10-04 20:39:39 +0100

    Lesson 99 :: ArrayInversionCount - refactor binary search into a method

--- 99-FutureTraining/ArrayInversionCount.cs
+++ 99-FutureTraining/ArrayInversionCount.cs
@@ -16,31 +16,7 @@ namespace Codility
 
             for (int i = 0; i < A.Length; i++)
             {
-                var low = 0;
-                var high = sortedNumbers.Count - 1;
-                var sortedIdx = -1;
-
-                while (low <= high)
-                {
-                    var mid = (low + high) / 2;
-
-                    // Binary search for the sorted index
-                    // of the first occurence of A[i]
-                    if (sortedNumbers[mid] == A[i])
-                    {
-                        sortedIdx = mid;
-                        high = mid - 1;
-                    }
-                    else if (sortedNumbers[mid] > A[i])
-                    {
-                        high = mid - 1;
-                    }
-                    else
-                    {
-                        low = mid + 1;
-                    }
-                }
-
+                var sortedIdx = GetFirstIndexOf(sortedNumbers, A[i]);
                 result += sortedIdx;
 
                 if (result > limit)
@@ -52,6 +28,36 @@ namespace Codility
             }
 
             return result;
+        }
+
+        private int GetFirstIndexOf(List<int> sortedNumbers, int num)
+        {
+            var low = 0;
+            var high = sortedNumbers.Count() - 1;
+            var index = -1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+
+                // Binary search for the sorted index
+                // of the first occurence of num
+                if (sortedNumbers[mid] == num)
+                {
+                    index = mid;
+                    high = mid - 1;
+                }
+                else if (sortedNumbers[mid] > num)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return index;
         }
     }
 }
