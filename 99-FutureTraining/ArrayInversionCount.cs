ec95bc04be5fd09bdc3fbdb9883100d104bb7a2d
Charles G <karol.sinx@gmail.com>
2015-10-06 21:10:41 +0100

    Lesson 99 :: Mergesort

--- 99-FutureTraining/ArrayInversionCount.cs
+++ 99-FutureTraining/ArrayInversionCount.cs
@@ -6,6 +6,8 @@ namespace Codility
 {
     public class ArrayInversionCount
     {
+        // Sort A and use binary search to
+        // find sorted indices
         public int solution(int[] A)
         {
             const int limit = (int)1e9;
@@ -58,6 +60,65 @@ namespace Codility
             }
 
             return index;
+        }
+
+        // Use modifed mergesort
+        public int solution2(int[] A)
+        {
+            Mergesort(A, 0, A.Length - 1);
+            return -1;
+        }
+
+        public void Mergesort(int[] A, int first, int last)
+        {
+            if (first >= last || first < 0 || last >= A.Length)
+            {
+                return;
+            }
+
+            var mid = (first + last) / 2;
+
+            // Divide array in half and sort recursively
+            Mergesort(A, first, mid);
+            Mergesort(A, mid + 1, last);
+
+            var sorted = new int[last - first + 1];
+            var sortedIdx = 0;
+            var leftIdx = first;
+            var rightIdx = mid + 1;
+
+            while (leftIdx <= mid && rightIdx <= last)
+            {
+                if (A[leftIdx] <= A[rightIdx])
+                {
+                    sorted[sortedIdx++] = A[leftIdx++];
+                }
+                else
+                {
+                    sorted[sortedIdx++] = A[rightIdx++];
+                }
+            }
+
+            if (leftIdx != mid + 1)
+            {
+                while (leftIdx <= mid)
+                {
+                    sorted[sortedIdx++] = A[leftIdx++];
+                }
+            }
+
+            if (rightIdx != last + 1)
+            {
+                while (rightIdx <= last)
+                {
+                    sorted[sortedIdx++] = A[rightIdx++];
+                }
+            }
+
+            for (int i = first; i <= last; i++)
+            {
+                A[i] = sorted[i - first];
+            }
         }
     }
 }
---
+++ 99-FutureTraining/Mergesort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Codility
+{
+    public static class Sort
+    {
+        public static void Mergesort(int[] A, int first, int last)
+        {
+            if (first >= last || first < 0 || last >= A.Length)
+            {
+                return;
+            }
+
+            var mid = (first + last) / 2;
+
+            // Divide array in half and sort recursively
+            Mergesort(A, first, mid);
+            Mergesort(A, mid + 1, last);
+
+            var sorted = new int[last - first + 1];
+            var sortedIdx = 0;
+            var leftIdx = first;
+            var rightIdx = mid + 1;
+
+            while (leftIdx <= mid && rightIdx <= last)
+            {
+                if (A[leftIdx] <= A[rightIdx])
+                {
+                    sorted[sortedIdx++] = A[leftIdx++];
+                }
+                else
+                {
+                    sorted[sortedIdx++] = A[rightIdx++];
+                }
+            }
+
+            if (leftIdx != mid + 1)
+            {
+                while (leftIdx <= mid)
+                {
+                    sorted[sortedIdx++] = A[leftIdx++];
+                }
+            }
+
+            if (rightIdx != last + 1)
+            {
+                while (rightIdx <= last)
+                {
+                    sorted[sortedIdx++] = A[rightIdx++];
+                }
+            }
+
+            for (int i = first; i <= last; i++)
+            {
+                A[i] = sorted[i - first];
+            }
+        }
+    }
+}
